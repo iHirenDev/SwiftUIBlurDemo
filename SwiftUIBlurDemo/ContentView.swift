dfee0bcb35de820e6141bf335402fd65fbde18ea
Hiren <patelhiren9857@gmail.com>
2019-11-26 11:41:54 +1100

    Initial Commit

---
+++ SwiftUIBlurDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBlurDemo
+//
+//  Created by Hiren Patel on 26/11/19.
+//  Copyright © 2019 com.hiren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
