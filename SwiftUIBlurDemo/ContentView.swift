441832e37643940a8a4594bf108346ac8d0e0dff
Hiren <patelhiren9857@gmail.com>
2019-11-26 12:28:32 +1100

    Initial commit

--- SwiftUIBlurDemo/ContentView.swift
+++ SwiftUIBlurDemo/ContentView.swift
@@ -9,9 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    //Declaring state for slidervalue
+    @State var sliderValue = 0.0
+    
+    //Minimum and maximum value for slider
+    var minValue = 0.0
+    var maxValue = 10.0
+    
+    
     var body: some View {
-        Text("Hello World")
-    }
+        VStack {
+                //Adding an Image with blur property in VStack
+                Image("swiftui")
+                    .blur(radius: CGFloat(self.sliderValue))
+            
+                //Adding HStack for the Slider
+                HStack{
+                    Text("0")
+                        .fontWeight(.bold)
+                        .padding()
+                    
+                    Slider(value: $sliderValue, in: minValue...maxValue)
+                        .padding()
+                    
+                    Text("10")
+                        .fontWeight(.bold)
+                        .padding()
+                    }
+                    
+                    Text("BlurRadius: \(Int(sliderValue))")
+                        .fontWeight(.bold)
+                        .font(.title)
+                }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
